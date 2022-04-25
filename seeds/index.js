203f57b534c0c461ae3e40fd224008aebcc7168e
SaiRev <saiyamjain1234567@gmail.com>
2022-04-25 12:44:04 +0530

    Done

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,15 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 mongoose
-  .connect("mongodb://localhost:27017/yelp-camp")
+  .connect(dbUrl)
   .then(() => {
     console.log("Mongo Connected");
   })
